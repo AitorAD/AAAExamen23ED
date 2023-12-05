d59830bc0025e28bf6054da7e21adb05f0fc206d
Aitor <aduqueagullo@gmail.com>
2023-12-05 13:11:12 +0100

    Añadido ha main.java las funciones sumar, multiplicar y potencia

--- main.java
+++ main.java
@@ -37,3 +37,20 @@ public class MostrarNumeros {
     } 
 
 }
+
+public static double sumar(double a, double b) { 
+
+        return a + b; 
+
+    } 
+public static double multiplicar(double a, double b) { 
+
+        return a * b; 
+
+    } 
+
+public static double potencia(double base, double exponente) { 
+
+        return Math.pow(base, exponente); 
+
+    } 
